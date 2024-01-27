ba2af415b00fa8906b501545a4f14d587bb9cac4
Misheck Lukhama <misheck.lukhama001@gmail.com>
2024-01-27 09:24:44 +0000

    added app localization

--- Documents/AngularProjects/flutter_projects/tontineo_app/lib/login_page.dart
+++ Documents/AngularProjects/flutter_projects/tontineo_app/lib/login_page.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   State<LoginPage> createState() => _LoginPageState();
-
 }
 
 class _LoginPageState extends State<LoginPage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- Documents/AngularProjects/flutter_projects/tontineo_app/lib/main.dart
+++ Documents/AngularProjects/flutter_projects/tontineo_app/lib/main.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:tontineo_app/tontineo_app.dart';
+import 'package:tontineo_app/tontineo_home_page.dart';
 
+import 'package:tontineo_app/l10n/app_localizations.dart'; // Adjust the import path
 
-void main() {
+void main() async {
   runApp(const TontineoApp());
 }
 
+class TontineoApp extends StatelessWidget {
+  const TontineoApp({super.key}); // Correct the syntax for the constructor
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [ // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English
+        const Locale('es', ''), // Spanish
+        const Locale('fr', ''), // French
+        // Add more supported locales as needed
+      ],
+      home:
+          TontineoHomePage(), // Replace TontineoAppHome() with your actual home page
+    );
+  }
+}
 
+class GlobalWidgetsLocalizations {}
+
+class GlobalMaterialLocalizations {}
+
+class AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizationsDelegate> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    // Update the list to include all the supported locales
+    return ['en', 'es', 'fr'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) {
+    return AppLocalizations.load(locale);
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate<AppLocalizations> old) {
+    return false;
+  }
+}
+
+class AppLocalizations {
+}
